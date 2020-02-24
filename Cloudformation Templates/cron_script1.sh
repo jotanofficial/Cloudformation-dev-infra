e966d434d54d8ac6d7cbc806f184e95a50415f65
jotanofficial <43163146+jotanofficial@users.noreply.github.com>
2020-02-24 14:08:25 +0800

    Add files via upload

---
+++ Cloudformation Templates/cron_script1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+crontab -r | { cat; echo '*/15 * * * * /home/ec2-user/checker_script.sh';echo '0 18 * * * /etc/init.d/httpd restart';echo '*/15 * * * * /usr/sbin/logrotate -f /etc/logrotate.d/';echo '*/5 * * * * ~/.aws-scripts-mon/mon-put-instance-data.pl --mem-util --mem-used --disk-space-util --disk-path=/ --from-cron'; } | crontab -
